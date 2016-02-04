81d3c987710fbb8c80f9d9945eb5f0fd62867d8e
Paul <paulsur@gmail.com>
2016-02-03 20:21:41 -0700

    Index.html shows play grid

--- js/app.js
+++ js/app.js
@@ -24,12 +24,28 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(dt) {
+
+};
+
+var player = new Player();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
